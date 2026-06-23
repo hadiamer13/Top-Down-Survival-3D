fb1565be69a0cab987becfea2af0710c69946e11
HadiUstaad <i253016@isb.nu.edu.pk>
2026-06-23 23:39:38 +0500

    KnockBack Added + animations
    enemy receives knockback when bullet hits. player walk animations

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -10,14 +10,20 @@ public class EnemyMovement : MonoBehaviour
     
 
     [Header("Enemy Attributes")]
-    [SerializeField] private float moveSpeed = 2;
+    [SerializeField] private bool isKnockbackEnabled = true;
+    [SerializeField] private bool isSquashEffectEnable = true;
+    [SerializeField] private float moveSpeed = 1;
+    [SerializeField] private float squashMagnitude = 0.2f;
+    [SerializeField] private float knockbackSpeed = 2f;
 
     private NavMeshAgent navMeshAgent;
     private GameObject Player;
+    private float knockbackease = 0.0001f;   // knockbackSpeed was goin into 0.001 so to make number realistic knockback ease is multiplied
     
 
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
         if(GameObject.FindWithTag("Player"))
         {
             Player = GameObject.FindWithTag("Player");
@@ -26,8 +32,10 @@ public class EnemyMovement : MonoBehaviour
         {
             Player = null;
         }
-            navMeshAgent = GetComponent<NavMeshAgent>();
-    
+       
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = moveSpeed;
+
 
     }
 
@@ -37,7 +45,8 @@ public class EnemyMovement : MonoBehaviour
     }
     private void MoveToPlayer()
     {
-        if(Player != null)
+        navMeshAgent.speed = moveSpeed;
+        if (Player != null)
         {
             navMeshAgent.destination = Player.transform.position;
             //Vector3 targetDirection = Player.transform.position - transform.position;
@@ -51,5 +60,36 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Weapon"))
+        {
+            if(isKnockbackEnabled)
+            {
+                // takes center of both objects to get direction
+                Vector3 knockbackDirection = transform.position - collision.transform.position;
+                Knockback(knockbackDirection);
+            }
+
+            if(isSquashEffectEnable)
+            {
+
+            }
+
+        }
+    }
+
+    private void Knockback(Vector3 knockbackDirection)
+    {
+        // top down game so no y movement
+        knockbackDirection.y = 0;
+
+        knockbackDirection = knockbackDirection.normalized;
+
+        navMeshAgent.speed = 0;
+        rb.AddForce(knockbackDirection * knockbackSpeed * knockbackease, ForceMode.Impulse);
+        navMeshAgent.speed = moveSpeed;
+    }
+
 }
+
